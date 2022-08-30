10bf62ae0ef2c282e340b4a878cf9a02d92aa35e
Jay Eneki <joanreneki@gmail.com>
2022-08-30 14:00:48 +0300

    Add migrations

---
+++ database/migrations/2022_08_30_102245_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->primary('id');
+            $table->string('name');
+            $table->string('author');
+            $table->string('img_url');
+            $table->string('file_url');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
